b5bd29d409a56b620d3e512f1783d4fbbf1789b4
user-lezi <leleziar@gmail.com>
2026-01-23 13:47:10 +0530

    Removed sec, csc and cot

--- dist/functions/trigonometry/cosec.d.ts
+++
@@ -1,4 +0,0 @@
-import { NativeFunction, ArgType } from "@tryforge/forgescript";
-declare const _default: NativeFunction<[import("@tryforge/forgescript").IArg<ArgType.Number, true, false, import("@tryforge/forgescript").EnumLike>, import("@tryforge/forgescript").IArg<ArgType.Boolean, boolean, false, import("@tryforge/forgescript").EnumLike>], true>;
-export default _default;
-//# sourceMappingURL=cosec.d.ts.map
